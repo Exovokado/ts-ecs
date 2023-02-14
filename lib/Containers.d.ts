d571b441bd404045acd54098eac3e66251a6c81e
Exovokado <46918700+Exovokado@users.noreply.github.com>
2023-02-14 14:16:02 +0100

    removed last build

--- lib/Containers.d.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from "./Component";
-/**
- * Structure holding all components of given entity, attach to by the ECS.
- */
-export declare class ComponentContainer {
-    map: Map<string, Component>;
-}
--- lib/Entity.d.ts
+++
@@ -1,6 +0,0 @@
-import { ComponentContainer } from "./Containers";
-/**
- * Simple unique reference. Uniqueness ensured by ECS.
- */
-export declare type Entity = string;
-export declare type Entities = Map<string, ComponentContainer>;
